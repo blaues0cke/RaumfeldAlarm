79ffe8278fb779545d48165a9748e274ace7a726
Thomas Kekeisen <thomas@thomaskekeisen.de>
2016-03-06 20:16:04 +0100

    Added a (example) script to reset the volume in all rooms

--- reset-volume.sh
+++ reset-volume.sh
@@ -8,11 +8,7 @@
 #          To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
 #
 
-# Put your alarm script here
-
-bash /usr/raumfeld/scripts/change-volume-by.sh -500
-
-initial_volume=0
+initial_volume=45
 
 bash /usr/raumfeld/scripts/change-room-volume-to.sh uuid:545683de-6070-4fb2-b178-cede3221ce45 $initial_volume # Bathroom
 bash /usr/raumfeld/scripts/change-room-volume-to.sh uuid:009bf27c-2027-4d76-b42b-924995495181 $initial_volume # Kitchen
@@ -20,23 +16,3 @@ bash /usr/raumfeld/scripts/change-room-volume-to.sh uuid:26e194ea-b46e-4b49-b4f1
 bash /usr/raumfeld/scripts/change-room-volume-to.sh uuid:27f5f37e-4eaa-4b7a-914f-73bd11d32788 $initial_volume # Sleeping room
 bash /usr/raumfeld/scripts/change-room-volume-to.sh uuid:6e297218-3c26-461d-a7b8-4a01be957e8d $initial_volume # Eating room
 bash /usr/raumfeld/scripts/change-room-volume-to.sh uuid:a4bf67ba-1d5e-4c93-a9f2-60ddd4a959e4 $initial_volume # Living room
-
-sleep 1
-
-bash /usr/raumfeld/scripts/tuneup-play-jahfari.sh
-
-# Sleep until advertising is over
-sleep 40
-
-# Thanks to
-# => http://stackoverflow.com/questions/169511/how-do-i-iterate-over-a-range-of-numbers-defined-by-variables-in-bash
-# 
-# Will increase the volume every 12 seconds by 1%, so the volume will be by 50% after 10 minutes
-for i in $(seq 1 50);
-do
-	echo "Increasing volume by 1, volume should now be ${i}"
-
-	bash /usr/raumfeld/scripts/change-volume-by.sh 1
-
-	sleep 12
-done
